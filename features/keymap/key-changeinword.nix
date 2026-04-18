42ab412aa4aeb8879cc5f4f5ddde1a52b483429f
Mark Lifantsev <mark.lifantsev@gmail.com>
2026-04-18 05:42:47 -0700

    added <angle bracket> text object

--- features/keymap/key-changeinword.nix
+++ features/keymap/key-changeinword.nix
@@ -11,6 +11,9 @@ in { keymaps = [
     { mode = modes; key = "${i}d"; action = "i\""; }
     { mode = modes; key = "${a}d"; action = "a\""; }
 
+    { mode = modes; key = "${i}a"; action = "i<"; }
+    { mode = modes; key = "${a}a"; action = "a<"; }
+
     { mode = modes; key = "${i}s"; action = "i["; }
     { mode = modes; key = "${a}s"; action = "a["; }
 ];}
