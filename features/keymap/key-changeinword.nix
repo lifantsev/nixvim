66fdd188f8ac71ee8e40cc8cfca344339cd7d159
Mark Lifantsev <mark.lifantsev@gmail.com>
2026-04-18 05:31:44 -0700

    made it respect keys.hjkl option

--- features/keymap/key-changeinword.nix
+++ features/keymap/key-changeinword.nix
@@ -1,11 +1,16 @@
-# operator pending remaps: ie ciw: change in word
-{ ... }: let modes = ["o" "v"]; in { keymaps = [
-    { mode = modes; key = "hs"; action = "i["; }
-    { mode = modes; key = "ks"; action = "a["; }
+# operator pending remaps like ciw: change in word
+{ lib, cfg, ... }: let
+    modes = [ "o" "v" ];
+    hjkl = cfg.keys.hjkl;
+    i = lib.findFirst (name: hjkl.${name} == "i") "i" (builtins.attrNames hjkl);
+    a = lib.findFirst (name: hjkl.${name} == "a") "a" (builtins.attrNames hjkl);
+in { keymaps = [
+    { mode = modes; key = "${i}q"; action = "i'"; }
+    { mode = modes; key = "${a}q"; action = "a'"; }
 
-    { mode = modes; key = "hd"; action = "i\""; }
-    { mode = modes; key = "kd"; action = "a\""; }
+    { mode = modes; key = "${i}d"; action = "i\""; }
+    { mode = modes; key = "${a}d"; action = "a\""; }
 
-    { mode = modes; key = "hq"; action = "i'"; }
-    { mode = modes; key = "kq"; action = "a'"; }
+    { mode = modes; key = "${i}s"; action = "i["; }
+    { mode = modes; key = "${a}s"; action = "a["; }
 ];}
