cea25d71c15e1819c79ae387ed0be882dca8eb8a
Mark Lifantsev <mark.lifantsev@gmail.com>
2026-04-25 18:47:35 -0700

    renamed remap to keymaps (bugfix)

--- features/vimplugin/winshift-nvim.nix
+++ features/vimplugin/winshift-nvim.nix
@@ -1,7 +1,7 @@
 { cfg, ... }: with cfg.keys.directional; {
     lua = /*lua*/ "require('winshift').setup()";
 
-    remap = [
+    keymaps = [
         { mode = "n"; key = "<leader>w${left}";  action = "<CMD>WinShift left<CR>"; }
         { mode = "n"; key = "<leader>w${up}";    action = "<CMD>WinShift up<CR>"; }
         { mode = "n"; key = "<leader>w${down}";  action = "<CMD>WinShift down<CR>"; }
